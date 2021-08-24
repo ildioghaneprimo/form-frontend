c58ab1d1e6ad5e4e38b60004a6d44a11745ecd45
Massimo <maxpiccinno18@gmail.com>
2021-08-24 14:04:52 +0200

    Aggiunto salvataggio dati in pdf

--- src/app/summary/summary.component.ts
+++ src/app/summary/summary.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { CompilazioniService } from '../compilazioni.service';
 const moment = require('moment');
+import { saveAs } from 'file-saver';
+import * as FileSaver from 'file-saver';
+import domtoimage from 'dom-to-image';
+const { jsPDF } = require('jspdf');
 
 @Component({
   selector: 'app-summary',
@@ -13,6 +17,7 @@ export class SummaryComponent implements OnInit {
   compilazioni: any[] = [];
   selectedComp: any;
   showInfo: boolean = false;
+  hidden: boolean = true;
 
   constructor(private compilazioniService: CompilazioniService) {}
 
@@ -36,5 +41,51 @@ export class SummaryComponent implements OnInit {
 
   formatDate(dateOriginal: any) {
     return moment(dateOriginal).format('DD/MM/YYYY');
+  }
+
+  saveFile() {
+    var node = document.getElementById('datiAtleta');
+
+    var img: HTMLImageElement;
+    var filename;
+    var newImage: string;
+    var selectedComp = this.selectedComp;
+
+    if (node != null)
+      domtoimage
+        .toPng(node, { bgcolor: '#fff' })
+
+        .then(function (dataUrl) {
+          img = new Image();
+          img.src = dataUrl;
+          newImage = img.src;
+
+          img.onload = function () {
+            var pdfWidth = img.width;
+            var pdfHeight = img.height;
+
+            var doc;
+
+            if (pdfWidth > pdfHeight) {
+              doc = new jsPDF('l', 'px', [pdfWidth, pdfHeight]);
+            } else {
+              doc = new jsPDF('p', 'px', [pdfWidth, pdfHeight]);
+            }
+
+            var width = doc.internal.pageSize.getWidth();
+            var height = doc.internal.pageSize.getHeight();
+
+            doc.addImage(newImage, 'PNG', 10, 10, width, height);
+            filename =
+              selectedComp.nomeAtleta +
+              '_' +
+              selectedComp.cognomeAtleta +
+              '.pdf';
+            doc.save(filename);
+          };
+        })
+        .catch(function (error) {
+          // Error Handling
+        });
   }
 }
